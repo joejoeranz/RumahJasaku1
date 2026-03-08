2acbf22d4d3668f99bdfd678f28000d702ab0314
joejoeranz <joejoeranz@gmail.com>
2026-03-09 05:45:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -21,23 +21,19 @@ const navbarNav = document.querySelector(".navbar-nav");
 
 function kirimWA(){
 
-let nama = document.getElementById("nama").value;
-let hp = document.getElementById("hp").value;
-let alamat = document.getElementById("alamat").value;
-let produk = document.getElementById("produk").value;
-let pembayaran = document.getElementById("pembayaran").value;
-let catatan = document.getElementById("catatan").value;
+let nama = document.getElementById("name").value;
+let hp = document.getElementById("phone").value;
+let alamat = document.getElementById("home").value;
+let pesan = document.getElementById("pesan").value;
 
-let pesan = "Halo Admin,%0A%0ASaya ingin melakukan pemesanan:%0A%0A"
+let text = "Halo Admin,%0A%0A"
 + "Nama: " + nama + "%0A"
 + "No HP: " + hp + "%0A"
 + "Alamat: " + alamat + "%0A"
-+ "Produk: " + produk + "%0A"
-+ "Pembayaran: " + pembayaran + "%0A"
-+ "Catatan: " + catatan;
++ "Pesan: " + pesan;
 
 let nomorAdmin = "6283852222174";
 
-window.open("https://wa.me/6283852222174" + nomorAdmin + "?text=" + pesan, "_blank");
+window.open("https://wa.me/" + nomorAdmin + "?text=" + text, "_blank");
 
 }
